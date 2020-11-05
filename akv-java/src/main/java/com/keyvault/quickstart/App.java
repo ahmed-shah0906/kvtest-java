2be2e55f24f6c0d7e5781c660cc78ad175477c37
Shahrukh Ahmed <shahrukh.ahmed@nagarro.com>
2020-11-05 14:43:42 +0530

    change secret name

--- akv-java/src/main/java/com/keyvault/quickstart/App.java
+++ akv-java/src/main/java/com/keyvault/quickstart/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) throws InterruptedException, IllegalArgumentException {
 
         String keyVaultName = "kv-az-demo-web";
-        String kvUri = "https://" + keyVaultName + ".vault.azure.net";
+        String kvUri = "https://" + keyVaultName + ".vault.azure.net/";
 
         System.out.printf("key vault name = %s and kv uri = %s \n", keyVaultName, kvUri);
 
@@ -24,7 +24,7 @@ public class App {
 
         System.out.println("Retrieving your secret from " + keyVaultName + ".");
 
-        KeyVaultSecret retrievedSecret = secretClient.getSecret("Hello");
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("SecretName");
 
         System.out.println("Your secret is '" + retrievedSecret.getValue() + "'.");
 
