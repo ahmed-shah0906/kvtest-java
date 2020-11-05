cfea59bb060cd7f4154d1f431efd1de3c5882b80
Shahrukh Ahmed <shahrukh.ahmed@nagarro.com>
2020-11-05 12:10:44 +0530

    Java code

---
+++ akv-java/src/main/java/com/keyvault/quickstart/App.java
@@ -0,0 +1,33 @@
+package com.keyvault.quickstart;
+
+import java.io.Console;   
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException, IllegalArgumentException {
+
+        String keyVaultName = "kv-az-demo-web";
+        String kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+        System.out.printf("key vault name = %s and kv uri = %s \n", keyVaultName, kvUri);
+
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(kvUri)
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+        System.out.println("Retrieving your secret from " + keyVaultName + ".");
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("Hello");
+
+        System.out.println("Your secret is '" + retrievedSecret.getValue() + "'.");
+
+        System.out.println("done.");
+    }
+}
